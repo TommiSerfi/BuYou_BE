be3f15c5da714d5e464b48bf033d1965370646f1
Serfilippi <tommaso.serfilippi@accenture.com>
2021-12-03 15:34:24 +0100

    primo commit

---
+++ src/main/java/com/buyou/BuYou/BuYouApplication.java
@@ -0,0 +1,13 @@
+package com.buyou.BuYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buyou/BuYou/BuYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyou.BuYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
